0a62de17f74b0a95892de61a5894de2034a756ee
Solomon Yakubu <soloyaks.sy@gmail.com>
2024-03-08 14:54:13 +0100

    first commit

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+  title: "Blog",
+  author: "Joe Smith",
+  headerTitle: "Next.js Blog",
+  description: "A blog created with Next.js, Tailwind.css and contentlayer.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://example.com", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "",
+  github: "https://github.com/homelessjoe1",
+  twitter: "https://twitter.com",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  linkedin: "https://www.linkedin.com/in",
+  dribbble: "https://www.dribbble.com",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
